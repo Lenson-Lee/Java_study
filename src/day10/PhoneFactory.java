32cb1a8065ed76b32aaa34c8b49682bcc1f6d0c3
Lenson-Lee <dmstjs7437@naver.com>
2021-08-10 10:51:29 +0900

    [21.08.10]클래스와 생성자

---
+++ src/day10/PhoneFactory.java
@@ -0,0 +1,56 @@
+package day10;
+//실행용 클래스( 객체를 생성해서 사용하는 곳)에 메인을 만든다.
+public class PhoneFactory {
+
+
+    public static void main(String[] args) {
+        //객체의 생성
+        Phone galaxyS21 = new Phone();
+        //new Phone(); 2개 이상으로 쓰면 여러개 만드ㅏ는거.
+        System.out.println("galaxyS21 = " + galaxyS21); //폰타입의 주소가 나타남
+
+        //객체의 속성과 기능 참조: 참조연산자 . 을 사용.(~를 통해, ~의 라는 뜻)
+        //ctrl 누르고 함수들 누르면 위치 이동함.
+//        galaxyS21.model = "갤럭시S21";
+//        galaxyS21.color = "크롬블랙";
+//        galaxyS21.price = 100000;
+//\       생성자가 값을초기화해서 설정안해도 된다.
+//          장젘) 기본적인 내용을 빠르게 대량생산.
+//          단점) 다른 폰 모델을 만들 수 없다.
+
+
+//        Phone iPhoneX = new Phone();
+//        iPhoneX.model = "아이폰X";
+//        iPhoneX.showSpec();
+//
+//        galaxyS21.powerOn();
+//        galaxyS21.showSpec();
+        //showSpec은 단독호출이 불가하다. 아무때나 쓸 수 있는게 아니라 우리가 설정한 phone만 사용가능한 함수다.
+        int a = 10;
+        //a.showSpec(); (x)
+
+
+        System.out.println("=================================");
+
+
+        Phone iPhoneX = new Phone("아이폰X");
+        iPhoneX.showSpec();
+
+        System.out.println("==================================");
+
+        Phone lgV6 = new Phone("LG V6", "다라이레드");
+        lgV6.showSpec();
+//        iPhoneX.model = "아이폰X";
+//        //iPhoneX.company => 필드에 없는 속성은 만들 수 없다. 원하면 필드에 가서 만들어야함.자바스크립트는 가능하지만 자바는 불가.
+//        iPhoneX.price = 200000;
+//
+//        iPhoneX.powerOn();
+//        iPhoneX.showSpec();
+//        galaxyS21.showSpec();
+
+
+
+
+
+    }//main end
+}
